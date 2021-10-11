385e5375acd642876635e2949287480ce2e5a938
JensBergChristensen <43059957+JensBergChristensen@users.noreply.github.com>
2021-10-11 08:33:15 +0200

    update

---
+++ Pythonting/PWM.py
@@ -0,0 +1,26 @@
+import time
+import RPi.GPIO as G
+G.setmode(G.BOARD)
+G.setup(40, G.OUT)
+G.setup(37, G.OUT)
+G.setup(33, G.OUT)
+
+G.output(33, G.HIGH)
+G.output(40, G.LOW)
+G.output(37, G.HIGH)
+
+
+p = G.PWM(37, 1000)
+p.start(0)
+try:
+    while 1:
+        for dc in range(0, 101, 5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+        for dc in range(100, -1, -5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+p.stop()
+G.cleanup()
