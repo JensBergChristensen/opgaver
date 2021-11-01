ad716d05954e3b043c09033c1395e4080b117115
JensBergChristensen <43059957+JensBergChristensen@users.noreply.github.com>
2021-11-01 11:40:58 +0100

    mappe

--- tema2/bilServer.py
+++ tema2/bilServer.py
@@ -16,9 +16,27 @@ pygame.display.set_caption("square move")
 done = False
 clock = pygame.time.Clock()
 
-# Send ting
-pakke = "Hejsa"
+# Send function
+def SEND(tast):
+    skt.sendto(tast, (host, port))
+
+# Send data
+pakke = "hejsa"
 pakke = pakke.encode("UTF-8")
+
+
+
+W = b'W'
+A = b'A'
+S = b'S'
+D = b'D'
+
+WA = b'WA'
+WD = b'WD'
+SA = b'SA'
+SD = b'SD'
+
+nul = b'0'
 
 while not done:
         for event in pygame.event.get():
@@ -29,6 +47,38 @@ while not done:
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_UP]:
             skt.sendto(pakke, (host, port))
+
+        # W Frem
+        if (pressed[pygame.K_w] and not pressed[pygame.K_a]) and (pressed[pygame.K_w] and not pressed[pygame.K_d]):
+            SEND(W)
+        elif pressed[pygame.K_w] and pressed[pygame.K_a]:
+            SEND(WA)
+        elif pressed[pygame.K_w] and pressed[pygame.K_d]:
+            SEND(WD)
+        else:
+            pass
+
+        # A Venstre
+        if (pressed[pygame.K_a] and not pressed[pygame.K_w]) and (pressed[pygame.K_a] and not pressed[pygame.K_s]):
+            SEND(A)
+        # D Højre
+        if (pressed[pygame.K_d] and not pressed[pygame.K_w]) and (pressed[pygame.K_d] and not pressed[pygame.K_s]):
+            SEND(D)
+        # S Tilbage
+        if (pressed[pygame.K_s] and not pressed[pygame.K_a]) and (pressed[pygame.K_s] and not pressed[pygame.K_d]):
+            SEND(S)
+        elif pressed[pygame.K_s] and pressed[pygame.K_a]:
+            SEND(SA)
+        elif pressed[pygame.K_s] and pressed[pygame.K_d]:
+            SEND(SD)
+        else:
+            pass
+
+        # Send nul hvis ingen knap er trykket
+        if pressed[pygame.K_w] or pressed[pygame.K_a] or pressed[pygame.K_s] or pressed[pygame.K_d]:
+            pass
+        else:
+            SEND(nul)
 
 
 
