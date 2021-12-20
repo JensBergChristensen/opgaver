62f95bac0e701b1ac005b27937056332a3032fba
JensBergChristensen <43059957+JensBergChristensen@users.noreply.github.com>
2021-12-20 13:27:40 +0100

    comments2

--- Projekt1/Vandspild.py
+++ Projekt1/Vandspild.py
@@ -1,8 +1,15 @@
 from gpizero import MCP3008 as mcp
-
+from time import sleep
 
 
 signal1 = mcp(channel=3)
 signal2 = mcp(channel=4)
 
+while true:
+    print("Signal1: ",signal1)
+    print("Signal2: ",signal2)
+    sleep(3)
+
+
+#Termometer skal have strom fra forskellige steder.
 #Gang signal med volt?
