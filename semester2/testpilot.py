30e5afa6ea690d398abca67463984fb045ff9091
JensBergChristensen <43059957+JensBergChristensen@users.noreply.github.com>
2022-04-19 08:34:33 +0200

    hejsa

--- semester2/testpilot.py
+++ semester2/testpilot.py
@@ -1,8 +1,8 @@
-# add two numbers
-
-
-def calc(a, b):
-    return a + b
-
-hej = calc(4, 5)
-print(hej)
+""" take user input, if it was a number higher than 10, open windows calculator """
+input_number = input("Please enter a number: ")
+input_number = int(input_number)
+if input_number > 10:
+    import os
+    os.system("calc")
+else:
+    print("Your number is not higher than 10")
