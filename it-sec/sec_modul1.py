dcf0e911e619dac39743313e6e7661ce2deeb614
JensBergChristensen <43059957+JensBergChristensen@users.noreply.github.com>
2023-09-04 13:45:15 +0200

    first

--- it-sec/sec_modul1.py
+++ it-sec/sec_modul1.py
@@ -1,2 +1,29 @@
 # test scripts
 
+'''
+# opgave 2
+print("opgave 2")
+
+input1 = input("input number: ")
+input2 = input("input number: ")
+
+print("input1 + input2 = ", int(input1) + int(input2))
+
+# opgave 3
+print("--------------------")
+print("opgave 3")
+
+s = input("text: ")
+print(s.translate({ord(i): None for i in ',.'}))
+
+
+# opgave 4
+print("--------------------")
+print("opgave 4")
+
+input4 = input("text: ")
+print(input4.upper())
+print(input4.lower())
+print(input4.title())
+'''
+
