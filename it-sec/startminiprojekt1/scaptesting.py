c7ccb0a69c5f5808d877327aa32443a3d807d2b1
JensBergChristensen <43059957+JensBergChristensen@users.noreply.github.com>
2024-02-05 12:57:15 +0100

    update

--- it-sec/startminiprojekt1/scaptesting.py
+++ it-sec/startminiprojekt1/scaptesting.py
@@ -1,6 +1,20 @@
-from scapy.all import ARP, Ether, srp
 
-# copied from article: https://thepythoncode.com/article/building-network-scanner-using-scapy
+# IMPORT STATEMENTS
+from scapy.all import ARP, Ether, srp
+import sqlite3
+
+# This script is used to scan the network for devices and store them in a database
+# network scanning copied from article: https://thepythoncode.com/article/building-network-scanner-using-scapy
+
+# create a database connection
+conn = sqlite3.connect('miniprojekt.db')
+c = conn.cursor()
+
+# create a table
+c.execute('''CREATE TABLE IF NOT EXISTS devices
+             (mac TEXT PRIMARY KEY, ip TEXT)''')
+
+
 
 target_ip = "192.168.1.1/24"
 # IP Address for the destination
@@ -26,3 +40,29 @@ print("Available devices in the network:")
 print("IP" + " "*18+"MAC")
 for client in clients:
     print("{:16}    {}".format(client['ip'], client['mac']))
+
+# insert the devices into the database
+for client in clients:
+    try:
+        c.execute("INSERT INTO devices (mac, ip) VALUES (?, ?)", (client['mac'], client['ip']))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print("Device already in database")
+
+
+
+# View table devices
+c.execute("SELECT * FROM devices")
+rows = c.fetchall()
+print("Devices in the database:")
+for row in rows:
+    print("MAC: {}, IP: {}".format(row[0], row[1]))
+
+
+# close the database connection
+conn.close()
+
+
+
+
+
